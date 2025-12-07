18109516752d1747f7de22d02818fc29a20b3d96
hoobasurf <brice.paneela@icloud.com>
2025-12-07 20:46:20 +0100

    Update app-owner.js

--- app-owner.js
+++ app-owner.js
@@ -1,3 +1,6 @@
+alert("app-owner.js exécuté !");
+console.log("db:", db, "storage:", storage);
+
 import { db, storage } from "./firebase.js";
 import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";

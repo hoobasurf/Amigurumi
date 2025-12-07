1afa06b8bac3e4a64f28bdffc1a15dc25ce4aaa0
hoobasurf <brice.paneela@icloud.com>
2025-12-07 11:24:07 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,19 @@
+// Import des modules Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
 
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-
+// Configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDbkbhXdZO20XdQpg3GhShFnqBVSpTdJKQ",
   authDomain: "amigurumi-2e7df.firebaseapp.com",
   projectId: "amigurumi-2e7df",
-  storageBucket: "amigurumi-2e7df.appspot.com", // ✔️ CORRECTION ICI !
+  storageBucket: "amigurumi-2e7df.appspot.com",
   messagingSenderId: "92443765428",
   appId: "1:92443765428:web:23e5aab383547b6f8885e1"
 };
 
+// Initialisation Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);

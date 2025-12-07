2e1081f2a00600e3a2c8e8ff46f6a598b964b1a6
hoobasurf <brice.paneela@icloud.com>
2025-12-07 20:33:48 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDbkbhXdZO20XdQpg3GhShFnqBVSpTdJKQ",
@@ -11,6 +10,5 @@ const firebaseConfig = {
   appId: "1:92443765428:web:23e5aab383547b6f8885e1"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const storage = getStorage(app, firebaseConfig.storageBucket);

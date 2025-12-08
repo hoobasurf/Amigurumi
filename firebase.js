76db47cf7d34b25a884d6b05c0310758c85da9f6
hoobasurf <brice.paneela@icloud.com>
2025-12-08 12:18:15 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,19 @@
-// Import Firebase core
+// Import des modules Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-
-// Firestore
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-
-// Storage (pour les photos)
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
 
-// --- CONFIGURATION FIREBASE (la tienne) ---
+// Configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDbkbhXdZO20XdQpg3GhShFnqBVSpTdJKQ",
   authDomain: "amigurumi-2e7df.firebaseapp.com",
   projectId: "amigurumi-2e7df",
-  storageBucket: "amigurumi-2e7df.firebasestorage.app",
+  storageBucket: "amigurumi-2e7df.appspot.com",   // ✔️ CORRIGÉ
   messagingSenderId: "92443765428",
   appId: "1:92443765428:web:23e5aab383547b6f8885e1"
 };
 
-// --- INITIALISATION ---
+// Initialisation Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);

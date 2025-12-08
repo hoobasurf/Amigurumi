6a5c41c1bfbb98470032d197ba47a1a455cec5cc
hoobasurf <brice.paneela@icloud.com>
2025-12-08 19:32:49 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,20 @@
-// Import des modules Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
+<!-- À mettre dans owner.html avant owner2.js -->
+<script src="https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js"></script>
+<script src="https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js"></script>
+<script>
+  // Configuration Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyDbkbhXdZO20XdQpg3GhShFnqBVSpTdJKQ",
+    authDomain: "amigurumi-2e7df.firebaseapp.com",
+    projectId: "amigurumi-2e7df",
+    storageBucket: "amigurumi-2e7df.appspot.com",
+    messagingSenderId: "92443765428",
+    appId: "1:92443765428:web:23e5aab383547b6f8885e1"
+  };
 
-// Configuration Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDbkbhXdZO20XdQpg3GhShFnqBVSpTdJKQ",
-  authDomain: "amigurumi-2e7df.firebaseapp.com",
-  projectId: "amigurumi-2e7df",
-  storageBucket: "amigurumi-2e7df.appspot.com",   // ✔️ CORRIGÉ
-  messagingSenderId: "92443765428",
-  appId: "1:92443765428:web:23e5aab383547b6f8885e1"
-};
-
-// Initialisation Firebase
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+  // Initialisation Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const storage = firebase.storage();
+</script>
